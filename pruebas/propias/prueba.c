0fdcf36a2500eb2e20d96704a99f7ce310f97a80
aaronespasa <aaespasa@gmail.com>
2023-05-05 12:26:45 +0200

    🧪 Initial tests

---
+++ pruebas/propias/prueba.c
@@ -0,0 +1,15 @@
+int vector[5];
+
+main ( ) {
+    printf("%s", vector);
+    int i = 0;
+    while (i < 5) {
+        vector[i] = i;
+        i = i + 1;
+    }
+    printf("%s", vector);
+    vector[0] = 123;
+    printf("%s", vector[0]);
+    printf("%s", vector);
+    printf("%s", vector[10]);
+}
